9de0b5aaf7d28e5c12bf1ab1b8e67efcac802124
Humanis-OS <contact@homohumanis-os.com>
2026-09-02 17:57:06 +0200

    Add files via upload

---
+++ .well-known/service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'cortexlab-v5-fixed';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
